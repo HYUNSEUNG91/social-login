86115f84fc02a3c399fa7606d990ee1bcb714f1c
HYUNSEUNG91 <lhs91001@naver.com>
2022-04-25 12:01:31 +0900

    Update index.js

--- schemas/index.js
+++ schemas/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connect = () => {
     mongoose
       .connect(
-        "mongodb://localhost:27017/Game",
+        "mongodb://localhost:27017/mapiaGame",
         // 이후 배포 시 변경 필요.
         { ignoreUndefined: true }
       )
