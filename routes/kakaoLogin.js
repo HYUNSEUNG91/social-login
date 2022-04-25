328d5d0879f5a487c676590a15a934f6e9c922a6
HYUNSEUNG91 <lhs91001@naver.com>
2022-04-25 17:41:01 +0900

    Update kakaoLogin.js

--- routes/kakaoLogin.js
+++ routes/kakaoLogin.js
@@ -8,7 +8,7 @@ const jwt = require("jsonwebtoken");
 
 const kakao = {
     clientid: `${process.env.CLIENTID}`, //REST API
-    redirectUri	: 'http://localhost3001/main'
+    redirectUri	: 'http://localhost:3001/main'
 }
 // kakao login page URL
 router.get('/kakaoLogin',(req,res)=>{
